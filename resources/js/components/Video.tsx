634a00c88edeca9a50c3e6ecb100437b8d2f74e8
lolbitto <rafae.juck@gmail.com>
2025-09-10 11:51:01 -0300

    Arrumando visual dos vídeos (não terminei)

--- resources/js/components/Video.tsx
+++ resources/js/components/Video.tsx
@@ -1,7 +1,7 @@
 export default function Video(props) {
     return (
-        <div class = "video inline-block">
-            <div class = "videoThumb">
+        <div className = "pt-10 mt-5 ml-5 mr-5 mb-5 rounded-xl w-90 h-110 inline-block">
+            <div className = "rounded-xl ml-10 w-80 h-11 bg-grey-200">
             </div>
             <p>{props.titulo}</p>
         </div>

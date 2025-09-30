91fcb2f1c06368cd919256d31ff51f456c836723
lolbitto <rafae.juck@gmail.com>
2025-09-30 08:23:37 -0300

    Colocando middlewares

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,21 @@ use Inertia\Inertia;
 
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\LoginController;
+use App\Http\Middleware\CheckLogin;
 
 Route::get('/', [MainController::class, 'index'])->name('home');
-Route::get('/login/', [LoginController::class, 'login']);
-Route::post('/loginsubmit', [LoginController::class, 'loginSubmit'])->name('login.submit');
-Route::get('/register/', [LoginController::class, 'register']);
+
+Route::middleware([CheckNotLogged::class])->group(
+    function() {
+        Route::get('/login/', [LoginController::class, 'login']);
+        Route::post('/loginsubmit', [LoginController::class, 'loginSubmit'])->name('login.submit');
+        Route::get('/register/', [LoginController::class, 'register']);
+    }
+);
+
+Route::middleware([CheckLogin::class])->group(
+    function() {
+        Route::get('/channel/', [LoginController::class, 'channel'])->name('user.home');
+    }
+);
+
